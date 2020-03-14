9a6a344e74653b1a7448e18dd44af5c2076a8646
RobertLowther <drail159@gmail.com>
2020-03-13 21:47:47 -0500

    Refactored astar method into maze class

--- Astar.py
+++ Astar.py
@@ -1,150 +1,175 @@
-'''
-This code is been pulled from https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2
-and should be used for educational purposes only.
-'''
-
 class Node():
     """A node class for A* Pathfinding"""
 
-    def __init__(self, parent=None, position=None):
+    def __init__(self, parent=None, position=None, blocked=False):
         self.parent = parent
         self.position = position
+        self.blocked = blocked
 
         self.g = 0
         self.h = 0
         self.f = 0
 
     def __eq__(self, other):
+        if not self or not other:
+            return False
         return self.position == other.position
 
+class Maze():
+    """A maze class for A* Pathfinding"""
 
-def astar(maze, start, end):
-    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+    def __init__(self, width = 0, height = 0):
+        self.width = width
+        self.height = height
+        self.nodes = []
 
-    # Create start and end node
-    start_node = Node(None, start)
-    end_node = Node(None, end)
+        for i in range(self.height):
+            nodes.append([])
+            for j in range(self.width):
+                nodes[i].append(Node(position = (i, j)))
 
-    # Initialize both open and closed list
-    open_list = []
-    closed_list = []
+    def __init__(self, maze):
+        self.width = len(maze[0])
+        self.height = len(maze)
+        self.nodes = []
 
-    # Add the start node
-    open_list.append(start_node)
+        for i in range(self.height):
+            self.nodes.append([])
+            for j in range(self.width):
+                self.nodes[i].append(Node(position = (i, j), blocked = maze[i][j] == 1))
 
-    # Loop until you find the end
-    while len(open_list) > 0:
+    def GetNode(self, pos):
+        if pos[0] >= 0 and pos[0] < len(self.nodes) and pos[1] >= 0 and pos[1] < len(self.nodes[0]):
+            return self.nodes[pos[0]][pos[1]]
 
-        # Get the current node
-        current_node = open_list[0]
-        current_index = 0
-        for index, item in enumerate(open_list):
-            if item.f < current_node.f:
-                current_node = item
-                current_index = index
+    def FindPath(self, start, end):
+        """Returns a list of tuples as a path from the given start to the given end in the given maze"""
 
-        # Pop current off open list, add to closed list
-        open_list.pop(current_index)
-        closed_list.append(current_node)
+        # Create start and end node
+        start_node = self.GetNode(start)
+        end_node = self.GetNode(end)
 
-        # Found the goal
-        if current_node == end_node:
-            path = []
-            current = current_node
-            while current is not None:
-                path.append(current.position)
-                current = current.parent
-            return path[::-1] # Return reversed path
+        if not start_node or not end_node:
+            print("Start and end node cannot be Nonetye")
+            return None
 
-        # Generate children
-        children = []
-        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+        # Initialize both open and closed list
+        open_list = []
+        closed_list = []
 
-            # Get node position
-            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+        # Add the start node
+        open_list.append(start_node)
 
-            # Make sure within range
-            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
-                continue
+        # Loop until you find the end
+        while len(open_list) > 0:
 
-            # Make sure walkable terrain
-            if maze[node_position[0]][node_position[1]] != 0:
-                continue
+            # Get the current node
+            current_node = open_list[0]
+            current_index = 0
+            for index, item in enumerate(open_list):
+                if item.f < current_node.f:
+                    current_node = item
+                    current_index = index
 
-            # Create new node
-            new_node = Node(current_node, node_position)
+            # Pop current off open list, add to closed list
+            open_list.pop(current_index)
+            closed_list.append(current_node)
 
-            # Append
-            children.append(new_node)
+            # Found the goal
+            if current_node == end_node:
+                path = []
+                current = current_node
+                while current is not None:
+                    path.append(current.position)
+                    current = current.parent
+                return path[::-1] # Return reversed path
 
-        # Loop through children
-        for child in children:
+            # Generate children
+            for new_position in [(-1, -1), (-1, 1), (1, -1), (1, 1), (0, -1), (0, 1), (-1, 0), (1, 0)]: # Adjacent squares
 
-            childClosed = False
-            # Child is on the closed list
-            for closed_child in closed_list:
-                if child == closed_child:
-                    childClosed = True
-                    break
+                # Get node position
+                next_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
-            if childClosed:
-                continue
+                # Get next node based on next position
+                next_node = self.GetNode(next_position)
 
-            # Create the f, g, and h values
-            child.g = current_node.g + 1
-            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
-            child.f = child.g + child.h
+                # Make sure within range
+                if not next_node or next_node.blocked or next_node in closed_list:
+                     continue
 
-            childOpen = False
-            # Child is already in the open list
-            for open_node in open_list:
-                if child == open_node and child.g > open_node.g:
-                    childOpen = True
-                    break
-            
-            if childOpen:
-                continue
+                newG = next_node.g + GetDeltaG(next_node.position, current_node.position)
 
-            # Add the child to the open list
-            open_list.append(child)
+                if next_node in open_list:
+                    if next_node.g < newG:
+                        continue
+                else:
+                    open_list.append(next_node)
+                    print()
 
+                # Set node parent and stats
+                next_node.g = newG
+                next_node.h = ((next_node.position[0] - end_node.position[0]) ** 2) + ((next_node.position[1] - end_node.position[1]) ** 2)
+                next_node.f = next_node.h + next_node.g
+                next_node.parent = current_node
+
+                #self.PrintMap(open_list, closed_list)
+                #print()
+
+    def PrintMap(self, open_list, closed_list):
+        print()
+        for i in range(self.height):
+            for j in range(self.width):
+                if self.nodes[i][j].blocked:
+                    print(" # ", end="")
+                elif self.nodes[i][j] in open_list:
+                    print(" 1 ", end="")
+                elif self.nodes[i][j] in closed_list:
+                    print(" 2 ", end="")
+                else:
+                    print(" 0 ", end="")
+            print()
+
+
+'''
+return the distance between two adjascent positions
+'''
+def GetDeltaG(pos1, pos2):
+    if pos1[0] != pos2[0] and pos1[1] != pos2[0]:
+        return 1.41
+    else:
+        return 1
 
 def main():
 
-    maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    mazeData = [[0, 0, 1, 0, 1, 1, 0, 0, 0, 0],
+                [0, 0, 1, 0, 1, 0, 0, 1, 1, 1],
+                [0, 0, 1, 0, 1, 1, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 1, 1, 1, 1, 0],
+                [0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 1, 1, 1],
+                [0, 0, 1, 0, 1, 1, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 1, 1, 1, 0, 0],
+                [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 1, 0, 0, 0, 0]]
+
+    maze = Maze(mazeData)
 
     start = (0, 0)
-    end = (0, 19)
+    end = (0, 9)
 
-    path = astar(maze, start, end)
+    path = maze.FindPath(start, end)
     print(path)
-    print()
-    for i in range(20):
-        for j in range(20):
-            if (i, j) in path:
-                print (" X ", end = "")
-            else:
-                print (f" {maze[i][j]} ", end = "")
-        print()
+
+    if path:
+        for i in range(maze.height):
+            for j in range(maze.width):
+                if (i, j) in  path:
+                    print (" X ", end = "")
+                elif maze.nodes[i][j].blocked == 1:
+                    print(" # ", end = "")
+                else:
+                    print(" . ", end = "")
+            print()
 
 
 if __name__ == '__main__':
